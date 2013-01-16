81b7d1d7b460375b3b34f34a12145701c4cb6b24
BramBoos <info@bram-koster.nl>
2013-01-16 13:53:39 +0100

    Bugfixes

--- main.js
+++ main.js
@@ -27,16 +27,23 @@ process.argv.forEach(function (val, index, array) {
 			if(val < 64) {
 				console.log('Too small packet size (at least 64 bytes)')
 				process.exit(1)
+			} else if(val > 65001) {
+				console.log('To big packet size (maximum = 65000 bytes)')
 			}
 			settings.size = val
 		}
 	}
 });
 
+function whileSend(){
+	client.send(message, 0, message.length, settings.server_port, settings.server_ip, function(err, size) {
+		return whileSend()
+	})
+}
+
 
 if(settings.mode == 'client') {
 	console.log('Client started')
-var ba = 0
 	var client = dgram.createSocket("udp4");
 	var timer = 0;
 	client.bind(settings.client_port);
@@ -60,22 +67,19 @@ var ba = 0
 		}
 			if(typeof(obj) == 'object') {
 				if(obj.done) {
-					console.log('Dit grapje koste mij '+timer+'ms')
+					console.log('Klaaaaar')
+					console.log(settings.amount + ' packets sent in ' + timer + ' ms')
+					//bytes/s
+					var total = settings.amount * settings.size;
+					var totalSizeSecond = Math.round(((total * 0.0009765625)/(timer/1000)))
+					console.log( totalSizeSecond + ' Kb/s (' + Math.round((totalSizeSecond * 0.009765625)) + ' Mb/s)')
 					process.exit(code=0)
 				}
 				if(obj.ready) {
-					console.log('Timer reset')
 					timer = 0;
 					setInterval(function(){ timer++ },1)
-					
-					for (var i=0;i<(settings.amount);i++) {
-						client.send(message, 0, message.length, settings.server_port, settings.server_ip, function(err, bytes){
-							
-						});
-						ba++;
-					}
-					console.log(ba)
-					console.log('Packets sent');
+					whileSend()
+					console.log('Sending packets');
 				}
 			}
 	})
@@ -90,9 +94,9 @@ if(settings.mode == 'server') {
 	server.on("message", function (msg, rinfo) {
 		if(rinfo.size > 60) {
 			console.log('Got a packet with size: ' + rinfo.size)
+			packets++;
 			console.log(packets)
 			console.log(settings.amount)
-			packets++;
 			if(packets >= settings.amount) {
 				var buf = new Buffer(JSON.stringify({'done': true}));
 				server.send(buf, 0, buf.length, settings.client_port, settings.client_ip)
@@ -105,13 +109,14 @@ if(settings.mode == 'server') {
 			}
 
 			if(typeof(setting) == 'object') {
-				console.log('asdfasd')
+				console.log('Got settings from client')
 				settings.amount = setting.amount
 				settings.ready = false
-				packets = 0;
+				packets = 0
 			}
 
 			if(!settings.ready) {
+				console.log('Send ready to send to client')
 				var buf = new Buffer(JSON.stringify({'ready': true}));
 				server.send(buf, 0, buf.length, settings.client_port, settings.client_ip)
 				settings.ready = true;
